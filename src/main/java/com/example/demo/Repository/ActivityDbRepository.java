4b520675f9dec3be6a2af646aa885e1139bd3289
Martine <martinemagh@gmail.com>
2018-09-27 15:04:31 +0200

    rediger

--- src/main/java/com/example/demo/Repository/ActivityDbRepository.java
+++ src/main/java/com/example/demo/Repository/ActivityDbRepository.java
@@ -3,8 +3,24 @@ package com.example.demo.Repository;
 import com.example.demo.Model.Activity;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class ActivityDbRepository implements ActivityInterface{
+
+
+    private List<Activity> activityList;
+
+    public ActivityDbRepository(){
+        activityList = new ArrayList<>();
+
+    }
+
+    @Override
+    public List<Activity> readAll() {
+        return activityList;
+    }
 
     @Override
     public Activity OpretAktivitet(Activity activity) {
@@ -14,6 +30,19 @@ public class ActivityDbRepository implements ActivityInterface{
 
     @Override
     public void RedigerAktivitet(Activity activity) {
+        activityList.remove(activity.getId()-1);
+        activityList.add(activity);
+
+
+
+//        jdbc.update("UPDATE AdventureDb.activities SET " +
+//                "name ='" + activity.getNavn() + "' , " +
+//                "age_limit ='" + activity.getAgeLimit() + "' , " +
+//                "pladser ='" + activity.getPladser() + "' , " +
+//                "sted ='" + activity.getSted() + "' , " +
+//                "date_time ='" + activity.getDateTime() + "' , " +
+//                "antal ='" + activity.getAntal() + "' WHERE activities_id = '" + activity.getId() + "'");
+
 
     }
 
--- src/main/java/com/example/demo/Repository/ActivityInterface.java
+++ src/main/java/com/example/demo/Repository/ActivityInterface.java
@@ -2,7 +2,11 @@ package com.example.demo.Repository;
 
 import com.example.demo.Model.Activity;
 
+import java.util.List;
+
 public interface ActivityInterface {
+
+    List<Activity> readAll();
 
     Activity OpretAktivitet(Activity activity);
 
