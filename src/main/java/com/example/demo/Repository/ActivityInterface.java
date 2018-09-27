252a8c3c73d297fae85acd713055dbf400acf6d6
Martine <martinemagh@gmail.com>
2018-09-27 15:07:42 +0200

    HEJ

--- src/main/java/com/example/demo/Repository/ActivityInterface.java
+++ src/main/java/com/example/demo/Repository/ActivityInterface.java
@@ -1,20 +1,14 @@
 package com.example.demo.Repository;
 
+import java.sql.SQLException;
 import com.example.demo.Model.Activity;
 
-<<<<<<< HEAD
-import java.sql.SQLException;
 import java.util.List;
 
 public interface ActivityInterface {
 
     List<Activity> readAll();
 
-    Activity OpretAktivitet(Activity activity);
-
-import java.sql.SQLException;
-
-public interface ActivityInterface {
 
     boolean OpretAktivitet(Activity activity) throws SQLException;
 
@@ -22,5 +16,6 @@ public interface ActivityInterface {
     void RedigerAktivitet(Activity activity);
 
     Activity LæsAktivitet(int id);
-
 }
+
+
